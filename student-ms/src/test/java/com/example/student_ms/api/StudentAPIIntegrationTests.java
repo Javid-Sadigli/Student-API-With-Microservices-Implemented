f17dea2c5e758da86c5cf585837b4db028d0f36c
Javid-Sadigli <cavidoffical@gmail.com>
2024-07-31 11:58:51 +0400

    Deleted POST Integration test

--- student-ms/src/test/java/com/example/student_ms/api/StudentAPIIntegrationTests.java
+++ student-ms/src/test/java/com/example/student_ms/api/StudentAPIIntegrationTests.java
@@ -1,15 +1,11 @@
 package com.example.student_ms.api;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
-import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,7 +17,6 @@ import com.example.student_ms.api.entity.StudentAPIEntity;
 import com.example.student_ms.api.feignclient.StudentAPI;
 import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
-
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @WireMockTest(httpPort = 8080)
@@ -64,44 +59,4 @@ public class StudentAPIIntegrationTests
         assertEquals(studentEmail, checkStudentEntity.getEmail());
         assertEquals(studentPassword, checkStudentEntity.getPassword());
     }
-
-    // @Test
-    // @DisplayName("Testing if StudentAPI can CREATE a student.")
-    // public void shouldSaveStudent(WireMockRuntimeInfo wireMockRuntimeInfo)
-    // {
-    //     Long studentId = 1L; 
-    //     String studentName = "Javid Sadigli", studentGender = "male";
-
-    //     stubFor(post(urlEqualTo("/api/student")).withRequestBody(equalToJson(String.format("""
-    //         {
-    //             "name": "%s", 
-    //             "gender": "%s"
-    //         }      
-    //     """, studentName, studentGender))).withHeader(
-    //         "Content-Type", equalTo("application/json")
-    //     ).willReturn(
-    //         aResponse().withStatus(201).withHeader("Content-Type", "application/json").withBody(
-    //             String.format("""
-    //                 {
-    //                     "id": %d,
-    //                     "name": "%s",
-    //                     "gender": "%s",
-    //                     "email": null,
-    //                     "password": null
-    //                 }
-    //             """, studentId, studentName, studentGender)
-    //         )
-    //     ));
-
-    //     StudentAPIEntity requestBody = new StudentAPIEntity(studentName, studentGender);
-    //     StudentAPIEntity checkStudentEntity = this.studentAPI.saveStudent(requestBody); 
-
-    //     assertNotNull(checkStudentEntity);
-    //     assertEquals(studentId, checkStudentEntity.getId());
-    //     assertEquals(studentName, checkStudentEntity.getName());
-    //     assertEquals(studentGender, checkStudentEntity.getGender());
-    //     assertNull(checkStudentEntity.getEmail());
-    //     assertNull(checkStudentEntity.getPassword());
-    // }
-
 }
